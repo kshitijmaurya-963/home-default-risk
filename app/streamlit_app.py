de3dc9d1e9cf887f84dbe1ed94e00a480afd152c
kshitijmaurya-963 <kshitijmaurya04@gmail.com>
2026-06-20 11:20:08 +0530

    Point streamlit to render api

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import math
 
-BASE_URL = "http://127.0.0.1:8000"
+BASE_URL = "https://home-credit-default-risk-api-ny4s.onrender.com"
 
 
 # ---------------------------------
